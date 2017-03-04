00129edf88e2d813805cf4f0cdd86c98c3e5fdc4
Adam Novotny <adam.novotny@postrock.wall>
2017-03-04 12:05:10 -0500

    fixed lsb function

--- steganography.py
+++ steganography.py
@@ -8,10 +8,6 @@ def decode_image(file_location="images/encoded_sample.png"):
     file_location: the location of the image file to decode. By default is the provided encoded image in the images folder
     """
     encoded_image = Image.open(file_location)
-    red_channel = encoded_image.split()[0]
-
-    print(red_channel.getpixel((1, 1)))
-    print(lsb_of_pixel(red_channel, 1, 1))
 
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
@@ -21,7 +17,7 @@ def decode_image(file_location="images/encoded_sample.png"):
 
     for x in range(x_size):
         for y in range(y_size):
-            if lsb_of_pixel(red_channel, x, y):
+            if lsb_of_red_pixel(encoded_image, x, y):
                 pixels[x, y] = (255,255,255)
             else:
                 pixels[x, y] = (0, 0, 0)
@@ -48,17 +44,33 @@ def write_text(text_to_write, image_size):
         offset += 10
     return image_text
 
-def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
+def encode_image(text_to_encode, template_image="images/samoyed.jpg", output_image="images/samoyed.secret.png"):
     """Encodes a text message into an image
 
     text_to_encode: the text to encode into the template image
     template_image: the image to use for encoding. An image is provided by default.
     """
-    pass #TODO: Fill out this function
 
+    image = Image.open(template_image)
+    pixels = image.load()
 
-def lsb_of_pixel(image, x, y):
-    return image.getpixel((x, y)) % 2
+    x_size = image.size[0]
+    y_size = image.size[1]
+
+    for x in range(x_size):
+        for y in range(y_size):
+            if lsb_of_red_pixel(image, x, y):
+                pixels[x,y] = (image.getpixel((x, y))[0] - 1, image.getpixel((x, y))[1], image.getpixel((x, y))[2])
+
+    text_image = Image.new("RGB", image.size)
+
+    usr_font = ImageFont.truetype("ComicNeue.otf", 25)
+    d_usr = ImageDraw.Draw(text_image)
+    d_usr = d_usr.text((105,280), text_to_encode,(0,0,0), font=usr_font)
+    text_image.show()
+
+def lsb_of_red_pixel(image, x, y):
+    return image.getpixel((x, y))[0] % 2
 
 if __name__ == '__main__':
     print("Decoding the image...")
